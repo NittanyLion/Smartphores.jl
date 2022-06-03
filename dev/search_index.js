c21525acdd554dd36433187f8554752fe999ab52
Documenter.jl <documenter@juliadocs.github.io>
2022-06-03 00:08:23 +0000

    build based on c46efe6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Smartphores","category":"page"},{"location":"#Smartphores","page":"Home","title":"Smartphores","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Smartphores.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Smartphores]","category":"page"}]
+}
